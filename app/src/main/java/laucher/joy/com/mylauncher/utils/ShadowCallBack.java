b1b0d3d179ae4517671883a51315dee67ee8c925
Hero <511656717@qq.com>
2016-02-26 17:28:59 +0800

    天气预报接口使用百度api;修改文件目录为JOYLauncher

--- app/src/main/java/laucher/joy/com/mylauncher/utils/ShadowCallBack.java
+++
@@ -1,8 +0,0 @@
-package laucher.joy.com.mylauncher.utils;
-
-public abstract interface ShadowCallBack
-{
-  public void startAnimation();
-  public void stopAnimation();
-    public void initView();
-}
